fbcad13cddbd99f86ebe23f23947c82117c2ece5
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-03-01 14:17:39 +0530

    setup leftSideMenu..

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LeftNav from "./LeftNav";
 
 function Feed() {
-  return <div>Feed</div>;
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)]">
+      <LeftNav />
+    </div>
+  );
 }
 
 export default Feed;
